221a753fd759754349941ec324d27f4b99f4a45d
newbion10 <96935020+newbion10@users.noreply.github.com>
2022-03-16 19:05:21 +0900

    Add files via upload

---
+++ assets/preload/data/interaction/SongName__Port.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', songName .. " | Friday Night Sanboxin' Psych engine port", 0, 2, 700); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
